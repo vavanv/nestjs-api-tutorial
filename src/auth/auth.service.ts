0a36c4d8137528e02d86f0fd9c25dc336d12e454
Vladimir V <vladimir.vorobiev@gmail.com>
2023-12-28 13:48:57 -0500

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { AuthDto } from 'src/dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    if (!pwMatches) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+
+    delete user.hash;
+    return user;
+  }
+}
